acd3ab832c9dd7295968e167b97457827c755bb4
Enes Fevzi <118919666+EnesFevzi@users.noreply.github.com>
2023-08-14 17:41:03 +0300

    Add files via upload

---
+++ OOP -Ornekler/01-OOPGirisClassTanimlamasi/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace _01_OOPGirisClassTanimlamasi
+{
+    public class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+            Utu utu1 = new Utu(); //  ==> instance yani örnek almak.....
+            utu1.Marka = "Rowenta";  //Property e değer atadık..
+            utu1.Renk = "Kırmızı";
+            utu1.KazanliMi = true;
+            utu1.Sicaklik = 0;
+            Console.WriteLine($"ÜTÜ NESNESİ AŞAĞIDAKİ BİLGİLERLE YARATILDI!\nMarkası:{utu1.Marka}\nRengi:{utu1.Renk}\nSıcaklık:{utu1.Sicaklik}\nKazanli mi? {(utu1.KazanliMi ? "Evet" : "Hayır")}");
+
+            Console.WriteLine();
+            Console.WriteLine(new string('*', 30));
+            Console.WriteLine();
+
+            Utu utu2 = new Utu("Arçelik", "Beyaz", 0, false);
+
+            Console.WriteLine();
+            Console.WriteLine(new string('*', 30));
+            Console.WriteLine();
+
+            Utu utu3 = new Utu("Bosch");
+            utu3.Renk = "Mavi";
+            utu3.Sicaklik = 0;
+            utu3.KazanliMi = true;
+
+            Console.WriteLine();
+            Console.WriteLine(new string('*', 30));
+            Console.WriteLine();
+
+            Utu utu4 = new Utu("Beko")
+            {
+                Renk = "Siyah",
+                KazanliMi = true,
+                Sicaklik = 0
+            };
+
+            Console.WriteLine();
+            Console.WriteLine(new string('*', 30));
+            Console.WriteLine();
+
+
+            Utu utu5 = new Utu
+            {
+                Marka = "Tefal",
+                Renk = "Siyah",
+                KazanliMi = true,
+                Sicaklik = 0
+            };
+
+            Console.WriteLine();
+            Console.WriteLine(new string('*', 30));
+            Console.WriteLine();
+
+
+            Utu utu6 = new Utu("Arzum", "Mor", 0, true);
+            utu6.Isin(50);
+
+
+
+            Console.WriteLine();
+            Console.WriteLine(new string('*', 30));
+            Console.WriteLine();
+
+
+            Utu utu7 = new Utu("Rowenta", null, default, default);
+            utu7.Isin(200);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOP -Ornekler/01-OOPGirisClassTanimlamasi/Utu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_OOPGirisClassTanimlamasi
+{
+    public class Utu
+    {
+        public string Marka { get; set; }
+        public string Renk { get; set; }
+        public int Sicaklik { get; set; }
+        public bool KazanliMi { get; set; }
+
+        //Boş Constructor oluşuyor eğer sadece boş yapı oluşturulursa instance alırken boş yapı çağrılabilir.Ancak parametreli yapı oluşturulurken hangisini çağıracağımızı bilmeli ve one göre parametre vermeli veya vermemeliyiz.Eğer hiç yapı tanımlanmazsa default olarak boş constructor varmış gibi davranır.
+        public Utu()
+        {
+            Console.WriteLine("Ütü nesnesi yaratıldı");
+        }
+        public Utu(string marka, string renk, int sicaklik, bool kazanliMi)
+        {
+            Marka = marka;
+            Renk = renk;
+            Sicaklik = sicaklik;
+            KazanliMi = kazanliMi;
+            Console.WriteLine($"ÜTÜ NESNESİ AŞAĞIDAKİ BİLGİLERLE YARATILDI!\nMarkası:{Marka}\nRengi:{Renk}\nSıcaklık:{Sicaklik}\nKazanli mi? {(KazanliMi ? "Evet" : "Hayır")}");
+        }
+
+        public Utu(string marka)
+        {
+            Marka = marka;
+
+            Console.WriteLine($"ÜTÜ NESNESİ AŞAĞIDAKİ BİLGİLERLE YARATILDI!\nMarkası:{Marka}");
+        }
+
+        //Bir class içinde davranışları metotlarla belirleyebiliriz.Bu metotlar dışarıdan erişime açık yada kapalı olabilir.Dışarıdan erişime açık ise instance alındıktan sonra ilgili nesne adına nokta konularak erişebilir.Dışarıdan erişime kapalıysa(private) nokta operatörü ile erişelemez.
+        public void Isin(int derece)
+        {
+            Sicaklik += derece;
+            Console.WriteLine($"ÜTÜ SICAKLIĞI ARTTI ŞU ANKİ SICAKLIK!:{Sicaklik}");
+        }
+    }
+}
