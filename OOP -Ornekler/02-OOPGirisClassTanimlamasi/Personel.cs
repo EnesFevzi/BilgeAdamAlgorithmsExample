5432c6c0d1156c9e1896d62c3a1b0e44b535c82f
Enes Fevzi <118919666+EnesFevzi@users.noreply.github.com>
2023-08-14 17:41:59 +0300

    Add files via upload

---
+++ OOP -Ornekler/02-OOPGirisClassTanimlamasi/Personel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_OOPGirisClassTanimlamasi
+{
+    public class Personel
+    {
+        //public Personel()
+        //{
+        //}
+        public int PersonelID { get; set; }
+        public string PersonelName { get; set; }
+        public string PersonelSurnname { get; set; }
+        public string Unvan { get; set; }
+        /* public decimal Maas { get; }*/ //==> set; metodu kaldırılırsa salt okunur(sadece okunur) bir property elde ederiz.
+
+        //Kapsülleme(Encapsulation) yapabimek için field-property yapısı birlikte kullanmalıyız.
+
+        private decimal _personelMaasi; //Field == Dışarıdan erişime kapalı olan alanlarımız. _öneki ile tanımlanır ve camelCasing kuralı uygulanır.
+
+        public decimal PersonelMaasi //Property == Dışarıdan erişime açık olan özelliklerimiz.Fielde erişmek veya fideldaki değeri set etmek için
+        {
+            get { return _personelMaasi; }
+            set
+            {
+                if (Unvan != "Mudur" || value < 11402.32M)
+                {
+                    Console.WriteLine("Yetkiniz yok ya da maaş değeri asgari ücretten düşük");
+                }
+                else
+                    _personelMaasi = value;
+            }
+        }
+
+    }
+}
---
+++ OOP -Ornekler/02-OOPGirisClassTanimlamasi/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02_OOPGirisClassTanimlamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel yeniPersonel = new Personel(); //new anahtar kelimesiyle yeniPersonel nesnesine Personel classın örneğini atıyoruz instance alıyoruz.
+            yeniPersonel.PersonelID = 1;
+            yeniPersonel.PersonelName = "Bilge";
+            yeniPersonel.PersonelSurnname = "Adam";
+            yeniPersonel.Unvan = "Mudur";
+            yeniPersonel.PersonelMaasi = 10000;
+
+
+            Console.WriteLine("Personel Bilgileri:"
+                + "\n----------------------------- \n"
+                + "\nPersonel ID :" + yeniPersonel.PersonelID
+                + "\nPersonel Adı :" + yeniPersonel.PersonelName
+                + "\nPersonel Soyadı :" + yeniPersonel.PersonelSurnname
+                + "\nPersonel Unvanı :" + yeniPersonel.Unvan
+                + "\nPersonel Maaşı :" + yeniPersonel.PersonelMaasi);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
